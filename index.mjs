281c28113231a569a27bdeb157a4975de6e2f571
Lucas Vale <lucasvale0904@gmail.com>
2024-10-21 18:44:03 -0300

    Commit inicial

---
+++ index.mjs
@@ -0,0 +1,13 @@
+import {log} from  './log.mjs';
+export const handler = async (event) => {
+    // TODO implement
+
+
+    log('Log de execução, event: ' + JSON.stringify(event));
+
+    return{
+        statusCode: 200,
+        body: JSON.stringify(event),
+    };
+  };
+  
